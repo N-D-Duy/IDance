ca6e5b2711d57ec82d02ca6885d0dad41f1b3773
Duy <nguyenducduy160903@gmail.com>
2023-05-09 21:37:44 +0700

    first commit

---
+++ app/src/main/java/com/example/idance/ui/PlayPortraitScreen.kt
@@ -0,0 +1,12 @@
+package com.example.idance.ui
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.idance.R
+
+class PlayPortraitScreen : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_play_portait)
+    }
+}
